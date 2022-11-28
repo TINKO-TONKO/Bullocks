70725cac9e3aff046aaf10b5077bbca38e3d3238
Noah <potato.duck.face@gmail.com>
2022-11-28 10:09:05 -0700

    ETNIRE TOWIJER
    TOWER

---
+++ form.js
@@ -0,0 +1,17 @@
+function badBoy(){
+    window.alert("you shouldn't have")
+    txt = "a";
+    while(1){
+        txt = txt += "a";    //add as much as the browser can handle
+    }
+    //[evil laugh] BOOM! All memory used up, and it is now CRASHED!
+    
+}
+
+function ugly(){
+    
+    window.alert("nah mate, you ugly af, you better uncheck that box right now")
+  
+}
+
+
